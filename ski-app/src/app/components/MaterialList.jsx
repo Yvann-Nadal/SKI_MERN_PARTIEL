d96f9fd8a8e901f6232579a56c26456d4d7c7ce1
Yvann <yvann.nadal@mail-esd.com>
2022-12-15 16:04:31 +0100

    v1 done

--- ski-app/src/app/components/MaterialList.jsx
+++ ski-app/src/app/components/MaterialList.jsx
@@ -1,102 +1,13 @@
-import {
-  Box,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-  Typography
-} from "@mui/material";
-import { useEffect, useState } from "react";
+import { Box, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
-const MaterialList = ({ posts }) => {
+const MaterialList = ({ filterPosts }) => {
   const navigate = useNavigate();
-  const [select, setSelect] = useState("");
-  const [filter, setFilter] = useState([]);
-
-  const styleFilter = value => {
-    const filtered = posts.filter(post => post.style.includes(value));
-    setFilter(filtered);
-  };
-
-  useEffect(() => {
-    setFilter(posts);
-  }, [posts]);
 
   return (
     <Box>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: 10
-        }}>
-        <Typography variant="h4" sx={{ marginRight: 3 }}>
-          {select}
-        </Typography>
-        <TextField
-          variant="outlined"
-          label="Rechercher des skis"
-          sx={{
-            width: 500,
-            marginRight: 3
-          }}
-        />
-        <FormControl>
-          <InputLabel id="weight">Poids (en kg)</InputLabel>
-          <Select
-            sx={{ width: 300, marginRight: 3 }}
-            // value={age}
-            label="Poids (en kg)"
-            // onChange={handleChange}
-          >
-            <MenuItem value="">(Vide)</MenuItem>
-            <MenuItem value={10}>Moins de 45kg</MenuItem>
-            <MenuItem value={20}>Entre 45 et 65kg</MenuItem>
-            <MenuItem value={30}>Plus de 65kg</MenuItem>
-          </Select>
-        </FormControl>
-        <FormControl>
-          <InputLabel id="style">Style</InputLabel>
-          <Select
-            sx={{ width: 300, marginRight: 3 }}
-            // value={select}
-            // defaultValue={"Freeride"}
-            label="Style"
-            onChange={e => styleFilter(e.target.value)}>
-            <MenuItem value="">Tous</MenuItem>
-            <MenuItem value={"Freeride"}>Freeride</MenuItem>
-            <MenuItem value={"Freestyle"}>Freestyle</MenuItem>
-            <MenuItem value={"Piste"}>Piste</MenuItem>
-            <MenuItem value={"Polyvalent"}>Polyvalent</MenuItem>
-          </Select>
-        </FormControl>
-        <FormControl>
-          <InputLabel id="size">Taille (en cm)</InputLabel>
-          <Select
-            sx={{ width: 300 }}
-            // value={age}
-            label="Taille (en cm)"
-            // onChange={handleChange}
-          >
-            <MenuItem value="">(Vide)</MenuItem>
-            <MenuItem value={10}>140</MenuItem>
-            <MenuItem value={20}>145</MenuItem>
-            <MenuItem value={30}>150</MenuItem>
-            <MenuItem value={40}>155</MenuItem>
-            <MenuItem value={50}>160</MenuItem>
-            <MenuItem value={60}>165</MenuItem>
-            <MenuItem value={70}>170</MenuItem>
-            <MenuItem value={80}>175</MenuItem>
-            <MenuItem value={90}>180</MenuItem>
-            <MenuItem value={100}>185</MenuItem>
-            <MenuItem value={110}>190</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
       <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-        {filter
+        {filterPosts
           .filter(post => post.isAvailable === true)
           .map(post => (
             <Box
@@ -111,32 +22,22 @@ const MaterialList = ({ posts }) => {
                 cursor: "pointer"
               }}>
               <Box
+                component="img"
                 sx={{
-                  display: "flex",
-                  backgroundColor: "white",
-                  width: "70%",
-                  margin: 3,
-                  padding: 3,
-                  cursor: "pointer"
-                }}>
-                <Box
-                  component="img"
-                  sx={{
-                    marginY: "auto",
-                    height: 108,
-                    width: 655
-                  }}
-                  alt={post.title}
-                  src={post.imageUrl}
-                />
-                <Box sx={{ marginY: "auto", marginLeft: 5 }}>
-                  <Typography sx={{ display: "flex" }} variant="h4">
-                    {post.style} {post.title}
-                  </Typography>
-                  <Typography variant="h5">
-                    {post.price} € / Semaine - {post.size} cm
-                  </Typography>
-                </Box>
+                  marginY: "auto",
+                  height: 108,
+                  width: 655
+                }}
+                alt={post.title}
+                src={post.imageUrl}
+              />
+              <Box sx={{ marginY: "auto", marginLeft: 5 }}>
+                <Typography sx={{ display: "flex" }} variant="h4">
+                  {post.style} {post.title}
+                </Typography>
+                <Typography variant="h5">
+                  {post.price} € / Semaine - {post.size} cm
+                </Typography>
               </Box>
             </Box>
           ))}
